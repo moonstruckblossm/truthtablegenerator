15f02ec9ac5c9fb48290faefa85c955ca230b802
moonstruckblossm <sm5069@rit.edu>
2021-08-03 12:51:13 -0400

    updated main...

---
+++ handleEq.py
@@ -0,0 +1,2 @@
+def evaluateExp(numTerms):
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-import handleEq.py
+'''Hi hello wow ok
+um hi my name's kai and um
+this is my truth table generator! at least, a very early version of it!
+currently, since this is for a project that needed to be put together within about three days,
+there isn't too much functionality!
+'''
+
+from handleEq import *
+
+def main():
+    #intro and information
+    print("Welcome to MARU'S TRUTH TABLE GENERATOR!")
+    print()
+    print("KEY\n"
+          "-'&'/'and' = LOGICAL AND\n"
+          "-'|'/'or' = LOGICAL OR\n"
+          "-'!'/'not = LOGICAL NOT/INVERT\n"
+          "-'^' = LOGICAL XOR\n"
+          "-you can put a ! in front of an 'and', 'or', or '^' (xor) expression to make a NAND, NOR "
+          "or XNOR function!")
+    print()
+    print("EXAMPLES")
+    print("!(A and B) | (B ^ C) || This is how you would enter (A NAND B) OR (B XOR C)")
+    #so you CAN pass in functions for your uhhhh eval
+
+    print()
+
+    #actually getting the equation lmao?
+    equation = input("Input a logical equation using the variables A, B, C, and D!")
