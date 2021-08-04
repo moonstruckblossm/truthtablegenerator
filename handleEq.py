0550ac3e2ca66ea95ac37c5fa3ec051dd3e303b4
moonstruckblossm <sm5069@rit.edu>
2021-08-04 13:55:47 -0400

    i literally have the world's largest brain (part 2)

--- handleEq.py
+++ handleEq.py
@@ -6,11 +6,23 @@ def evaluateExp(numVar, equation):
     numLines = 2 ** numVar
 
     #create a dictionary that contains numVar letters of the alphabet, all equal to False
-
+    varDict = {}
+    for i in range(numVar):
+        varDict[ALPHABET[i]] = False
+    print(varDict)
     #break the equation up into a list since strings aren't mutable
+    eqList = list(equation)
 
     #replace all characters that are CAPITAL LETTERS (in ALPHABET) with dict[that letter]
+    for i in range(len(eqList)):
+        if 65 <= ord(eqList[i]) <= 90:
+            eqList[i] = "varDict["+eqList[i]+"]"
+    print(eqList)
 
-    #put the string back together and THEN evaluate
+    #put the string back together
+    newEqString = "".join(eqList)
+    print(newEqString)
+
+    #
 
     #my brain is freaking massive
