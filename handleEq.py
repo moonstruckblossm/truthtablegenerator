fd35defa56ea266f0d7bd1921b56d6c0657fc544
moonstruckblossm <sm5069@rit.edu>
2021-08-04 12:33:52 -0400

    i literally have the world's largest brain

--- handleEq.py
+++ handleEq.py
@@ -1,2 +1,16 @@
-def evaluateExp(numTerms):
-    pass
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def evaluateExp(numVar, equation):
+    # now to find the number of lines in the actual table using
+    # numlines = 2^numVar
+    numLines = 2 ** numVar
+
+    #create a dictionary that contains numVar letters of the alphabet, all equal to False
+
+    #break the equation up into a list since strings aren't mutable
+
+    #replace all characters that are CAPITAL LETTERS (in ALPHABET) with dict[that letter]
+
+    #put the string back together and THEN evaluate
+
+    #my brain is freaking massive
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 um hi my name's kai and um
 this is my truth table generator! at least, a very early version of it!
 currently, since this is for a project that needed to be put together within about three days,
-there isn't too much functionality!
+there isn't too much functionality! but i promise it's the cutest truth table generator you'll ever see!
 '''
 
 from handleEq import *
@@ -10,28 +10,36 @@ from utilityFunctions import *
 
 def main():
     #intro and information
-    print("Welcome to MARU'S TRUTH TABLE GENERATOR!")
+    print("\033[1;95;40m♡ Welcome to MARU'S TRUTH TABLE GENERATOR! ♡\033[0m")
     print()
-    print("KEY\n"
-          "-'&'/'and' = LOGICAL AND\n"
-          "-'|'/'or' = LOGICAL OR\n"
-          "-'!'/'not = LOGICAL NOT/INVERT\n"
-          "-'^' = LOGICAL XOR\n"
-          "-you can put a ! in front of an 'and', 'or', or '^' (xor) expression to make a NAND, NOR "
+    print("\033[4;96;40m♡ KEY ♡\033[0m\n"
+          " \n"
+          "♡ '&' or 'and' = LOGICAL AND\n"
+          "♡ '|' or 'or' = LOGICAL OR\n"
+          "♡ '!' or 'not' = LOGICAL NOT/INVERT\n"
+          "♡ '^' = LOGICAL XOR\n"
+          "♡ you can put a ! in front of an 'and', 'or', or '^' (xor) expression to make a NAND, NOR "
           "or XNOR function!")
     print()
-    print("EXAMPLES")
-    print("!(A and B) | (B ^ C) || This is how you would enter (A NAND B) OR (B XOR C)")
+    print("\033[4;96;40m♡ EXAMPLES ♡\033[0m\n")
+    print("♡ !(A and B) | (B ^ C) || This is how you would enter (A NAND B) OR (B XOR C)")
     #so you CAN pass in functions for your uhhhh eval
 
     print()
 
     #actually getting the equation lmao?
 
-    #for now we can assume that the number of vars the user snters is accurate to the yknow ACTUAL NUMBER
-    equation = input("Input a logical equation using capital variables such as A, B, C, D, etc: ")
+    print("\033[1;93;40mlet's get started! ☆\033[0m\n")
 
-    #finds the amount of variables in the equation
-    print(findGreatest(equation), "variables detected!")
+    #for now we can assume that the number of vars the user snters is accurate to the yknow ACTUAL NUMBER
+    equation = input("♡ Input a logical equation using capital variables such as A, B, C, D, etc: ")
+
+    #finds the amount of variables in the equation.
+    #for now we can assume that, if a letter is entered, the user has entered all of the letters preceding it
+    numVar = findGreatest(equation)
+    print(numVar, "variables detected!")
+
+    #finally, we can actually pass in the number of lines and the equation!
+    evaluateExp(numVar, equation)
 
 main()
